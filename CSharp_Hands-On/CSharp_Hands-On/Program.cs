752a2ff29012870bfad29c1e819f4dfac41dcd5c
PIAI21JUL3006 <tanushreekohad01@gmail.com>
2022-04-12 23:41:31 +0530

    Added Hands-on

---
+++ CSharp_Hands-On/CSharp_Hands-On/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp_Hands_On
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxMinElement maxmin = new MaxMinElement();
+            maxmin.MaxMin();
+
+            MaxOccuringElement maxOccurence = new MaxOccuringElement();
+            maxOccurence.MaxOccuring();
+
+            PositionOfWord findpos = new PositionOfWord();
+            findpos.FindPosition();
+
+        }
+    }
+}
+
+
